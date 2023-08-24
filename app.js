13fc4a8d87b9f92a89a4160953a7a664ccc6a85f
EwenLeFloch <ewenlelf@gmail.com>
2023-08-24 17:31:18 +0200

    controllers books and .env in gitignore

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+const Book = require("./models/books.model");
 
 // Utilisation de morgan pour le logging des requêtes
 app.use(morgan("combined"));
@@ -23,37 +24,10 @@ app.use((req, res, next) => {
 	next();
 });
 
-// Route pour créer un nouvel objet
-app.post("/api/books", (req, res, next) => {
-	console.log(req.body);
-	res.status(201).json({
-		message: "Objet créé !",
-	});
-});
-
-// Route pour obtenir une liste d'objets
-app.get("/api/books", (req, res, next) => {
-	const books = [
-		{
-			_id: "1",
-			title: "Mon premier objet",
-			description: "Les infos de mon premier objet",
-			imageUrl:
-				"https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-			price: 4900,
-			userId: "qsomihvqios",
-		},
-		{
-			_id: "2",
-			title: "Mon deuxième objet",
-			description: "Les infos de mon deuxième objet",
-			imageUrl:
-				"https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-			price: 2900,
-			userId: "qsomihvqios",
-		},
-	];
-	res.status(200).json(books);
+// Gestion des erreurs
+app.use((error, req, res, next) => {
+	console.error("Error", error);
+	res.status(500).json({ error: "Une erreur est survenue" });
 });
 
 module.exports = app;
