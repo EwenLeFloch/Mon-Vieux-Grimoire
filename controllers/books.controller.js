8d76f75447bd590cf93d040690e3eaede60a351b
EwenLeFloch <ewen.lefloch96@gmail.com>
2023-08-28 18:15:06 +0200

    controller books

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,1 +1,40 @@
 const Book = require("../models/books.models");
+
+//To create a new book
+exports.createBook = (req, res, next) => {
+	delete req.body._id;
+	const book = new Book({
+		...req.body,
+	});
+	book.save()
+		.then(() => res.status(201).json({ message: "Objet enregistre !" }))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+//To find one book
+exports.getOneBook = (req, res, next) => {
+	Book.findOne({ _id: req.params.id })
+		.then((books) => res.status(200).json(books))
+		.catch((error) => res.status(404).json({ error }));
+};
+
+//To find all books
+exports.getAllBooks = (req, res, next) => {
+	Book.find()
+		.then((books) => res.status(200).json(books))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+//To modify a book
+exports.modifyBook = (req, res, next) => {
+	Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Objet modifie !" }))
+		.catch((error) => res.status(400).json({ error }));
+};
+
+//To delete a book
+exports.deleteBook = (req, res, next) => {
+	Book.deleteOne({ _id: req.params.id })
+		.then((books) => res.status(200).json({ message: "Objet supprime !" }))
+		.catch((error) => res.status(404).json({ error }));
+};
--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,45 +1,12 @@
 const express = require("express");
 
 const router = express.Router();
-const Book = require("../models/books.model");
+const booksControllers = require("../controllers/books.model");
 
-// Poster un nouveau livre
-router.post("/", (req, res, next) => {
-	delete req.body._id;
-	const book = new Book({
-		...req.body,
-	});
-	book.save()
-		.then(() => res.status(201).json({ message: "Objet enregistre !" }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-// Recuperer tous les livres
-router.get("/" + "", (req, res, next) => {
-	Book.find()
-		.then((books) => res.status(200).json(books))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-//Recuperer un seul livre
-router.get("/:id", (req, res, next) => {
-	Book.findOne({ _id: req.params.id })
-		.then((books) => res.status(200).json(books))
-		.catch((error) => res.status(404).json({ error }));
-});
-
-// Modifier un livre existant
-router.put("/:id", (req, res, next) => {
-	Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-		.then(() => res.status(200).json({ message: "Objet modifie !" }))
-		.catch((error) => res.status(400).json({ error }));
-});
-
-// Supprimer un livre existant
-router.delete("/:id", (req, res, next) => {
-	Book.deleteOne({ _id: req.params.id })
-		.then((books) => res.status(200).json({ message: "Objet supprime !" }))
-		.catch((error) => res.status(404).json({ error }));
-});
+router.get("/", booksControllers.getAllBooks);
+router.post("/", booksControllers.createBook);
+router.get("/:id", booksControllers.getOneBook);
+router.put("/:id", booksControllers.modifyBook);
+router.delete("/:id", booksControllers.deleteBook);
 
 module.exports = router;
